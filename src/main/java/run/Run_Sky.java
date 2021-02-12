243a4c7d6fd78e3379cd40d1462e8054d096770e
Augusto Marinho <augustoaclm@gmail.com>
2021-02-12 12:36:52 -0300

    remove target

--- src/main/java/run/Run_Sky.java
+++ src/main/java/run/Run_Sky.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features = "src/main/resources",
+@CucumberOptions( features = "src/main/resources", 
 glue = {"steps", "hooks"},
 plugin = {"pretty", "json:target/cucumber-reports/teste.json"},
 monochrome = true)
